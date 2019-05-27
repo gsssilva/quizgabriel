9ff78143f26aa88597709b2c51f75bf820359987
Gabriel Silva <gsssilva@users.noreply.github.com>
2019-05-27 14:46:51 -0300

    quiz

---
+++ questions.js
@@ -0,0 +1,71 @@
+var questions = [
+//EX.01
+    ["(Enem 2018)  Uma empresa deseja iniciar uma campanha publicitária divulgando uma promoção para seus possíveis consumidores. Para esse tipo de campanha, os meios mais viáveis são a distribuição de panfletos na rua e anúncios na rádio local. Considera-se que a população alcançada pela distribuição de panfletos seja igual à quantidade de panfletos distribuídos, enquanto que a alcançada por um anúncio na rádio seja igual à quantidade de ouvintes desse anúncio. O custo de cada anúncio na rádio é de R$ 120,00 e a estimativa é de que seja ouvido por 1.500 pessoas. Já a produção e a distribuição dos panfletos custam R$ 180,00 cada 1.000 unidades. Considerando que cada pessoa será alcançada por um único desses meios de divulgação, a empresa pretende investir em ambas as mídias. Considere X e Y os valores (em real) gastos em anúncios na rádio e com panfletos, respectivamente. O número de pessoas alcançadas pela campanha será dado pela expressão",  
+    "", "", "", "", "",
+     "", "img/1a.png", "img/1b.png", "img/1c.png", "img/1d.png", "img/1e.png",
+        "1"
+    ],
+
+//EX.02
+    ["(Enem 2018) Numa atividade de treinamento realizada no Exército de um determinado país, três equipes – Alpha, Beta e Gama – foram designadas a percorrer diferentes caminhos, todos com os mesmos pontos de partida e de chegada. - A equipe Alpha realizou seu percurso em 90 minutos com uma velocidade média de 6,0kmh. - A equipe Beta também percorreu sua trajetória em 90 minutos, mas sua velocidade média foi de 5,0kmh. - Com uma velocidade média de 6,5kmh, a equipe Gama concluiu seu caminho em 60 minutos. Com base nesses dados, foram comparadas as distâncias Dbeta; Dalpha e Dgama percorridas pelas três equipes. A ordem das distâncias percorridas pelas equipes Alpha, Beta e Gama é:",  
+    "", "", "", "", "",
+     "", "img/2a.png", "img/2b.png", "img/2c.png", "img/2d.png", "img/2e.png",
+        "1"
+    ],
+
+//EX.03
+    ["(Enem 2018) Um mapa é a representação reduzida e simplificada de uma localidade. Essa redução, que é feita com o uso de uma escala, mantém a proporção do espaço representado em relação ao espaço real. Certo mapa tem escala 1:58.000.000. Considere que, nesse mapa, o segmento de reta que liga o navio à marca do tesouro meça 7,6cm. A medida real, em quilômetro, desse segmento de reta é",  
+    "4.408", "7.632", "44.080", "76.316", "440.800",
+     "img/fto3.png", "", "", "", "", "",
+        "1"
+    ],
+
+//EX.04
+    ["(Enem 2017) O resultado de uma pesquisa eleitoral, sobre a preferência dos eleitores em relação a dois candidatos, foi representado por meio do Gráfico 1. Ao ser divulgado esse resultado em jornal, o Gráfico 1 foi cortado durante a diagramação, como mostra o Gráfico 2. Apesar de os valores apresentados estarem corretos e a largura das colunas ser a mesma, muitos leitores criticaram o formato do Gráfico 2 impresso no jornal, alegando que houve prejuízo visual para o candidato B. A diferença entre as razões da altura da coluna B pela coluna A nos gráficos 1 e 2 é",  
+    "0", "", "", "", "",
+     "img/e4.png", "", "img/4b.png", "img/4c.png", "img/4d.png", "img/4e.png",
+        "5"
+    ],
+
+//EX.05
+    ["(Enem 2016) Cinco marcas de pão integral apresentam as seguintes concentrações de fibras (massa de fibra por massa de pão): - Marca A: 2g de fibras a cada 50g de pão; - Marca B: 5g de fibras a cada 40g de pão; - Marca C: 5g de fibras a cada 100g de pão; - Marca D: 6g de fibras a cada 90g de pão; - Marca E: 7g de fibras a cada 70g de pão. Recomenda-se a ingestão do pão que possui a maior concentração de fibras. A marca a ser escolhida é:",  
+    "A", "B", "C", "D", "E",
+     "", "", "", "", "", "",
+    "2"
+    ],
+
+//EX.06
+    ["(Enem 2016) No tanque de um certo carro de passeio cabem até 50L de combustível, e o rendimento médio deste carro na estrada é de 15kmL de combustível. Ao sair para uma viagem de 600km o motorista observou que o marcador de combustível estava exatamente sobre uma das marcas da escala divisória do medidor, conforme figura a seguir. Como o motorista conhece o percurso, sabe que existem, até a chegada a seu destino, cinco postos de abastecimento de combustível, localizados a 150km,187km,450km,500km e 570km do ponto de partida. Qual a máxima distância, em quilômetro, que poderá percorrer até ser necessário reabastecer o veículo, de modo a não ficar sem combustível na estrada?",  
+    "570", "500", "450", "187", "150",
+     "img/e6.png", "", "", "", "", "",
+    "2"
+    ],
+
+//EX.07
+    ["(Enem 2016) Em uma empresa de móveis, um cliente encomenda um guarda-roupa nas dimensões 220cm de altura, 120cm de largura e 50cm de profundidade. Alguns dias depois, o projetista, com o desenho elaborado na escala 1:8, entra em contato com o cliente para fazer sua apresentação. No momento da impressão, o profissional percebe que o desenho não caberia na folha de papel que costumava usar. Para resolver o problema, configurou a impressora para que a figura fosse reduzida em 20%. A altura, a largura e a profundidade do desenho impresso para a apresentação serão, respectivamente:",  
+    "22,00cm, 12,00cm e 5,00cm.", "27,50cm, 15,00cm e 6,50cm.", "34,37cm, 18,75cm e 7,81cm.", "35,20cm, 19,20cm e 8,00cm.", "44,00cm, 24,00cm e 10,00cm.",
+     "", "", "", "", "", "",
+    "1"
+    ],
+
+//EX.08
+    ["(Enem 2016) De forma geral, os pneus radiais trazem em sua lateral uma marcação do tipo abc/deRfg, como 185/65R15. Essa marcação identifica as medidas do pneu da seguinte forma: - abc é a medida da largura do pneu, em milímetro; - de é igual ao produto de 100 pela razão entre a medida da altura (em milímetro) e a medida da largura do pneu (em milímetro); - R significa radial; - fg é a medida do diâmetro interno do pneu, em polegada. A figura ilustra as variáveis relacionadas com esses dados. O proprietário de um veículo precisa trocar os pneus de seu carro e, ao chegar a uma loja, é informado por um vendedor que há somente pneus com os seguintes códigos: 17565R15, 17575R15, 17580R15, 18560R15 e 20555R15. Analisando, juntamente com o vendedor, as opções de pneus disponíveis, concluem que o pneu mais adequado para seu veículo é o que tem a menor altura. Desta forma, o proprietário do veículo deverá comprar o pneu com a marcação ",  
+    "205/55R15.", "175/65R15.", "175/75R15.", "175/80R15.", "185/60R15.",
+    "img/e8.png", "", "", "", "", "",
+    "5"
+    ],
+
+//EX.09
+    ["(Enem 2016) Um paciente necessita de reidratação endovenosa feita por meio de cinco frascos de soro durante 24h. Cada frasco tem um volume de 800mL de soro. Nas primeiras quatro horas, deverá receber 40% do total a ser aplicado. Cada mililitro de soro corresponde a 12 gotas. O número de gotas por minuto que o paciente deverá receber após as quatro primeiras horas será",  
+    "16", "20", "24", "34", "40",
+     "", "", "", "", "", "",
+    "3"
+    ],
+
+//EX.10
+    ["Ao final, descarta-se o filtro com a maior razão entre a medida da massa de contaminantes não capturados e o número de dias, o que corresponde ao de pior desempenho. O filtro descartado é o (Enem 2016) Diante da hipótese do comprometimento da qualidade da água retirada do volume morto de alguns sistemas hídricos, os técnicos de um laboratório decidiram testar cinco tipos de filtros de água. Dentre esses, os quatro com melhor desempenho serão escolhidos para futura comercialização. Nos testes, foram medidas as massas de agentes contaminantes, em miligrama, que não são capturados por cada filtro em diferentes períodos, em dia, como segue: - Filtro 1 (F1): 18mg em 6 dias; - Filtro 2 (F2): 15mg em 3 dias; - Filtro 3 (F3): 18mg em 4 dias; - Filtro 4 (F4): 6mg em 3 dias; - Filtro 5 (F5): 3mg em 2 dias.",  
+    "F1", "F2", "F3", "F4", "F5",
+     "", "", "", "", "", "",
+    "2"
+    ],
+]
